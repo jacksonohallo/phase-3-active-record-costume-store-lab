50e9f5886b30212df4f6b76f36a0a923dbd6f69a
jacksonohallo <jacksonohallo@gmail.com>
2022-08-28 12:41:56 +0300

    commit tree b5c4001d5fa27c5afd5128ec03036cdd020c0c6e

--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,17 @@
-# Create your HauntedHouse class here
+
+# Create your HauntedHouse class h
+
+class HauntedHouse < ActiveRecord::Base
+
+ def change
+create_table :haunted_houses do|t|
+    t.string :name
+    t.string :location
+    t.string :theme
+    t.float  :family_friendly
+    t.datetime :closing_date
+    t.datetime  :opening_date
+    t.text    :description
+end
+ end
+end    
